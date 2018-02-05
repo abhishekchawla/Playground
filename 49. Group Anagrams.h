57cb814b17ee01700b52a769aca5944ad308b716
Abhishek Chawla <abhishek2410@live.in>
2018-02-04 23:35:41 -0500

    Added new solutions

--- 49. Group Anagrams.h
+++ 49. Group Anagrams.h
@@ -1,3 +1,32 @@
 #pragma once
 #include "Header.h"
 
+//Given an array of strings, group anagrams together.
+//
+//For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+//Return :
+//
+//	[
+//		["ate", "eat", "tea"],
+//		["nat", "tan"],
+//		["bat"]
+//	]
+//Note: All inputs will be in lower - case.
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+	vector<vector<string>> ret;
+	unordered_map<string, vector<string>> umap;
+	for (string str : strs)
+	{
+		string s = str;
+		sort(s.begin(), s.end());
+		umap[s].push_back(move(str));
+	}
+	for (auto itr : umap)
+		ret.push_back(move(itr.second));
+	return ret;
+
+
+
+}
