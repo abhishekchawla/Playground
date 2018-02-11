bc75dec6365d72902f4150aef3d271a96bd74d88
Abhishek Chawla <abhishekchawla@users.noreply.github.com>
2018-02-10 23:57:14 -0500

    Update 347. Top K Frequent Elements.h

--- 347. Top K Frequent Elements.h
+++ 347. Top K Frequent Elements.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "Header.h"
 
+/*Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.*/
 
 vector<int> topKFrequent(vector<int>& nums, int k)
 {
